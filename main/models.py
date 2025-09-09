2c106d98d278e6e6f739f94efc0c31ea13727f56
Aforambis <rusydanmujtabaa@gmail.com>
2025-09-10 00:39:51 +0700

    Added Bid class to set some fundamental bidding rules

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import uuid
 
 class AuctionSeason(models.Model):
@@ -18,10 +19,11 @@ class AuctionSeason(models.Model):
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('jersey', 'Jersey'),
-        ('shoes', 'Shoes'),
-        ('memorabilia', 'Memorabilia'),
-        ('collectible', 'Collectible')
+        ('jersey', 'Match-Worn Jersey'),
+        ('shoes', 'Match-Worn Shoes'),
+        ('ball', 'Match Ball'),
+        ('artwork', 'Artwork & Posters'),
+        ('other', 'Other'),
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -30,20 +32,17 @@ class Product(models.Model):
     # Fields wajib sesuai ketentuan soal
     name = models.CharField(max_length=255)
     price = models.IntegerField()
-    description = models.TextField(max_length=255)
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(default=0)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)    
     is_featured = models.BooleanField(default=False)
-
     club = models.CharField(max_length=100, blank=True, null=True)  
     player = models.CharField(max_length=100, blank=True, null=True) 
     match_date = models.DateField(blank=True, null=True)             
     
     def __str__(self):
-        if self.club:
-            club_info = self.club
-        else: ""
-        return f"{self.name} {club_info} - Starts from Rp{self.price:,.0f}"
+        club_info = f"{self.club}" if self.club else club_info = ""
+        return f"{self.name} {club_info} - Starts from IDR {self.price:,.0f}"
     
     @property
     def highest_bid(self):
@@ -53,3 +52,16 @@ class Product(models.Model):
     def current_price(self):
         highest = self.highest_bid
         return highest.amount if highest else self.price
+    
+class Bid(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-amount', 'created_at']
+
+    def __str__(self):
+        return f"{self.user.username} bids IDR {self.amount:,.0f} on {self.product.name}"
