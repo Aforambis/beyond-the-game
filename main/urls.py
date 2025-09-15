b315138b40b2bd73138e9b3d0772ea6373d38a6b
Aforambis <rusydanmujtabaa@gmail.com>
2025-09-15 21:33:56 +0700

    Menambahkan path untuk keempat method xml & json di urls.py projek main, serta membenarkan nama method show_json_by_xml menjadi show_xml_by_id

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import show_main
+from .views import show_main, show_json, show_xml, show_json_by_id, show_xml_by_id
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('json/', show_json, name="show_json"),
+    path('xml/', show_xml, name="show_xml"),
+    path('json/<uuid:id>', show_json_by_id, name="show_json"),
+    path('xml/<uuid:id>', show_xml_by_id, name="show_xml_by_id")
 ]
