bb7f09843f326eff1cede79c26c18100689548e3
Aforambis <rusydanmujtabaa@gmail.com>
2025-10-08 05:14:00 +0700

    Fixed static URL for deployment

--- beyond_the_game/settings.py
+++ beyond_the_game/settings.py
@@ -142,12 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Memberitahu Django di mana folder 'static' utama kamu berada saat development
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Direktori tempat `collectstatic` akan mengumpulkan semua file statis untuk production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
